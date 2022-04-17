3a098bdceef04ee371b76a7dc1cba8c0e19a1ce4
Arko Mandal <arkomandal@Arkos-MacBook-Air.local>
2022-04-17 09:12:58 +0530

    Build: environment url modification

--- dist/main.js
+++ dist/main.js
@@ -76,7 +76,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    baseUrl: "https://fake-rest.glitch.me/"
+    baseUrl: "https://api.arkomandal.com/fake-rest/"
 };
 /*
  * For easier debugging in development mode, you can import the following file
